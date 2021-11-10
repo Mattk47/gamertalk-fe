34be425a7b356b84eb8b99f16ed06b5919b2a917
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-10 13:32:28 +0000

    create myReviews component

--- src/components/GameReview.js
+++ src/components/GameReview.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getReviewById, patchVote, deleteReview, getCommentByReview } from '../utils'
+import { getReviewById, patchVote, getCommentByReview } from '../utils'
 import { Redirect } from 'react-router-dom'
 import Expandable from '../Expandable';
+import { Button } from '@mui/material';
+import { Save } from '@mui/icons-material';
 
 const GameReview = () => {
     const [review, setReview] = useState({})
@@ -10,6 +12,7 @@ const GameReview = () => {
     const [redirect, setRedirect] = useState();
     const [comments, setComments] = useState([])
     const { review_id } = useParams()
+
     useEffect(() => {
         const getReview = getReviewById(review_id)
         const getComment = getCommentByReview(review_id)
@@ -20,39 +23,48 @@ const GameReview = () => {
 
     }, [review_id])
 
+    useEffect(() => {
+        window.localStorage.setItem('newVote', newVote);
+    }, [newVote]);
+
+    console.log(review_id)
+
     const updateVote = () => {
-        if (newVote === 0) {
-            setNewVote((currVote) => currVote + 1)
+        const vote = JSON.parse(window.localStorage.getItem('newVote'));
+        console.log(vote)
+        if (vote === 0) {
+            setNewVote(1)
             patchVote(review_id, 1).then()
         } else {
-            setNewVote((currVote) => currVote - 1)
+            setNewVote(0)
             patchVote(review_id, -1).then()
         }
     }
-    const removeReview = () => {
-        deleteReview(review_id).then(res => setRedirect(true))
-    }
+
     if (redirect) return <Redirect to={`/`} />
+
     return (
         <div>
-            <h2>{review.title}</h2>
-            <div align="right">
-                <button className="deleteButton " onClick={removeReview}>Delete</button>
-            </div>
-            <img className="review_image" alt={review.category} src={review.review_img_url} />
-            <p>{review.review_body}</p>
-            <p>Author: {review.owner}</p>
+            <h2 className="reviewTitle review__text">{review.title}</h2>
+            <img className="review_image " alt={review.category} src={review.review_img_url} />
+            <p className='text--margin review__text'>{review.review_body}</p>
+            <p className='review__text'>Author:<strong> {review.owner}</strong> </p>
+
             <h3 >&#8679;{review.votes + newVote}</h3>
-            <button className="upvoteButton" onClick={updateVote}>Upvote</button>
+
+            {newVote ? <Button variant='contained' color='primary' style={{ color: 'white', borderColor: 'rgb(26, 33, 46)', border: '2px solid', backgroundColor: 'rgb(26, 33, 46)' }} onClick={updateVote}>Upvote</Button>
+                : <Button variant='outlined' color='primary' style={{ color: 'rgb(26, 33, 46)', borderColor: 'rgb(26, 33, 46)', border: '2px solid' }} onClick={updateVote}>Upvote</Button>
+            }
+
             <h2>Comments</h2>
-            <Expandable>
+            <Expandable reviewId={review_id}>
                 <ul>
                     {
                         comments.map(comment => {
                             return (
-                                <li className="comment" key={comment.comment_id}>
+                                <li className="commentBox" key={comment.comment_id}>
                                     <p><strong>{comment.author}</strong></p>
-                                    <p>{comment.body}</p>
+                                    <p className="text--margin">{comment.body}</p>
                                 </li>
                             )
                         })
