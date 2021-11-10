c8fc4d8378372bb90d5dc3cff908f6adb9720330
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-11-10 13:22:18 +0000

    create postComment utils func

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
-export const getReviews = (sort_by, order, page, category) => {
+export const getReviews = (sort_by, order, page, category, limit) => {
     return axios.get("https://nc-games-project.herokuapp.com/api/reviews", {
         params: {
             sort_by,
             order,
             page,
             category,
-            limit: 5
+            limit
         },
     })
 }
@@ -34,4 +34,12 @@ export const deleteReview = (review_id) => {
 export const getCommentByReview = (review_id) => {
     return axios.get(`https://nc-games-project.herokuapp.com/api/reviews/${review_id}/comments`)
 
+}
+
+export const getReviewsByUsername = (username) => {
+    return axios.get(`https://nc-games-project.herokuapp.com/api/users/reviews/${username}`)
+}
+
+export const postComment = (review_id, username, comment,) => {
+    return axios.post(`https://nc-games-project.herokuapp.com/api/reviews/${review_id}/comments`, { username, body: comment })
 }
