495ae55d94ee3db8b638d14f9f583946735a8b92
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2022-01-02 22:51:28 +0000

    add reducer types

--- src/context/Review/reviewReducer.js
+++ src/context/Review/reviewReducer.js
@@ -1,19 +1,35 @@
-import { GET_REVIEWS, SET_REVIEW_FILTER, SET_CATEGORY_FILTER, GET_CATEGORIES, SET_PAGE, SET_LOADING } from "../types"
+import {
+    GET_REVIEWS,
+    SET_REVIEW_FILTER,
+    SET_CATEGORY_FILTER,
+    GET_CATEGORIES,
+    SET_PAGE,
+    SET_LOADING,
+    ADD_REVIEW,
+    REMOVE_REVIEW,
+    GET_USER_REVIEWS,
+    SET_REVIEW,
+    DELETE_REVIEW,
+    SET_COMMENTS,
+    ADD_COMMENT,
+    TOGGLE_OPEN
+} from "../types"
 
-export default (state, action) => {
+const reviewReducer = (state, action) => {
     switch (action.type) {
         case GET_REVIEWS:
             return {
                 ...state,
                 reviewList: action.payload.reviews,
                 loading: false,
-                totalGames: action.payload.total_count
+                totalGames: action.payload.total_count,
+                review: {}
             }
         case SET_REVIEW_FILTER:
 
             return {
                 ...state,
-                reviewFilter: action.payload
+                reviewFilter: action.payload,
             }
         case SET_CATEGORY_FILTER:
             return {
@@ -24,7 +40,21 @@ export default (state, action) => {
             return {
                 ...state,
                 categories: action.payload,
-                loading: false
+                loading: false,
+                review: {}
+
+            }
+        case SET_COMMENTS:
+            return {
+                ...state,
+                comments: action.payload,
+                loading: false,
+            }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                comments: [...state.comments, action.payload],
+                loading: false,
             }
         case SET_PAGE:
             return {
@@ -37,8 +67,44 @@ export default (state, action) => {
                 ...state,
                 loading: true
             }
+        case ADD_REVIEW:
+        case SET_REVIEW:
+            return {
+                ...state,
+                userReviews: [...state.userReviews, action.payload],
+                review: action.payload,
+                loading: false,
+            }
+
+        case REMOVE_REVIEW:
+            return {
+                ...state,
+                review: {},
+                loading: false
+            }
+        case DELETE_REVIEW:
+            return {
+                ...state,
+                userReviews: state.userReviews.filter(review => review.review_id !== action.payload),
+                review: {},
+                loading: false
+            }
+        case GET_USER_REVIEWS:
+            return {
+                ...state,
+                userReviews: action.payload,
+                loading: false,
+                user: {}
+            }
+        case TOGGLE_OPEN:
+            return {
+                ...state,
+                isOpen: !state.isOpen
+            }
         default:
             return state
     }
 
 }
+
+export default reviewReducer;
